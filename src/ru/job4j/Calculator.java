d4920a264e5017f9d4be955183545b8a2d013f27
AnreyUlitin <77831775+AnreyUlitin@users.noreply.github.com>
2021-01-30 21:16:01 +0300

    Update Calculator.java

--- src/ru/job4j/Calculator.java
+++ src/ru/job4j/Calculator.java
@@ -1,21 +1,16 @@
-package ru.job4j;
+public class ArgMethod {
 
-public class Calculator {
-
+    public static void hello(String name) {
+        System.out.println("Hello, " + name);
+    }
+    public static void hello1(String age) {
+        System.out.println("Age " + age);
+    }
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int onePlusTwo = one + two;
-        System.out.println(onePlusTwo);
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int sixDivTwo = six/two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
+        String name = "Petr Arsentev";
+        String age = "33";
+        ArgMethod.hello(name);
+        ArgMethod.hello1(age);
     }
 }
 
