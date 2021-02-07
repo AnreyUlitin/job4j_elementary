5f7fabb92fe2bd28da21c9c2465974fa19cfd8ce
AnreyUlitin <77831775+AnreyUlitin@users.noreply.github.com>
2021-02-07 21:47:15 +0300

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,23 @@
+package ru.job4j.Calculator;
+
+import org.junit.Assert;
+
+public @interface Test1 {
+
+    @Test1
+    public void whenA1B1C1X1Then3() {
+        /* Входные параметры. Их будет 4 штуки */
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        /* ожидаемое значение. Это всегда одна переменная. */
+        int expected = 3;
+        /* вызов метода, который хотим проверить. */
+        /* Результат вычисления. Это всегда одна переменная. */
+
+        int rsl = X2.calc(a, b, c, x);
+        /* сравнение полученного значения с ожидаемым */
+        Assert.assertEquals(expected, rsl);
+    }
+}
