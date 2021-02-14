52c7e4786e6c616da50f462cacb070176d7b4938
AnreyUlitin <77831775+AnreyUlitin@users.noreply.github.com>
2021-02-14 13:07:59 +0300

    Add files via upload

--- src/ru/job4j/LogicNot.java
+++ src/ru/job4j/LogicNot.java
@@ -4,27 +4,32 @@ public class LogicNot {
 
     public boolean isEven(int num) {
         return num % 2 == 0;
+
     }
 
     public boolean isPositive(int num) {
         return num > 0;
+
     }
 
     public boolean notEven(int num) {
         return !isEven(num);
+
     }
 
     public boolean notPositive(int num) {
         return !isPositive(num);
+
     }
 
     public boolean notEvenAndPositive(int num) {
-        if (!(num % 2 == 0) && num > 0)
-            return notEvenAndPositive(num);
+        return !isEven(num) && isPositive(num);
+
     }
 
     public boolean evenOrNotPositive(int num) {
-        if ((num % 2 == 0) && !(num > 0))
-            return  evenOrNotPositive(num);
+        return !isEven(num) && !isPositive(num);
+
     }
+
 }
