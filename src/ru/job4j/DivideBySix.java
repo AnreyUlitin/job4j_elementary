17998acaeeca067c16d1d5b2d53311c8d59869fa
AnreyUlitin <77831775+AnreyUlitin@users.noreply.github.com>
2021-02-15 21:28:09 +0300

    Add files via upload

---
+++ src/ru/job4j/DivideBySix.java
@@ -0,0 +1,22 @@
+package ru.job4j.Condition;
+
+public class DivideBySix {
+    public class DivideBySix {
+        public static String checkNumber(int number) {
+            String rsl;
+            if (...) {
+                if (...) {
+                    rsl = ...;
+                } else {
+                    rsl = ...;
+                }
+            } else {
+                if (...) {
+                    rsl = ...;
+                } else {
+                    rsl = ...;
+                }
+            }
+            return rsl;
+        }
+}
