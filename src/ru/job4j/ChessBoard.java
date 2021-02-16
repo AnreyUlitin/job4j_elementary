bd57a64fa3a8b06671999a1562e654d292d6a9fb
AnreyUlitin <77831775+AnreyUlitin@users.noreply.github.com>
2021-02-16 09:09:55 +0300

    Update ChessBoard.java

--- src/ru/job4j/ChessBoard.java
+++ src/ru/job4j/ChessBoard.java
@@ -3,19 +3,16 @@ package ru.job4j.condition;
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
         int rsl = 0;
-        if (x1 > x2 || y1 > y2) {
+        if (Math.abs(x2 - x1) == Math.abs(y2 - y1))  {
             rsl = Math.abs(x2 - x1);
-            rsl = Math.abs(y2 - y1);
-            rsl = rsl == 0 ? Math.abs(x2 - x1) : rsl;
-            rsl = rsl == 0 ? Math.abs(y2 - y1) : rsl;
-        }
-        return   rsl;
+                                }
+        return  rsl;
     }
 
-    public static void main(String[] args){
+public static void main(String[] args){
         System.out.println(way(6,7,1,2));
         System.out.println(way(7,0,0,7));
         System.out.println(way(2,6,4,1));
 
-    }
-}
+        }
+        }
