4d133871b269fb2b1354e8b98f44971954bf110f
AnreyUlitin <77831775+AnreyUlitin@users.noreply.github.com>
2021-02-16 21:15:53 +0300

    Add files via upload

--- src/ru/job4j/DivideBySix.java
+++ src/ru/job4j/DivideBySix.java
@@ -1,22 +1,28 @@
 package ru.job4j.Condition;
 
-public class DivideBySix {
     public class DivideBySix {
         public static String checkNumber(int number) {
             String rsl;
-            if (...) {
-                if (...) {
-                    rsl = ...;
+            if (number % 3 == 0 && number % 2 == 0) {
+                if (number % 3 == 0 && number / 2 == 0) {
+                    rsl = "Исходное число делится на 6.";
                 } else {
-                    rsl = ...;
+                    rsl = "Исходное число делится на 3, но не является четным.";
                 }
             } else {
-                if (...) {
-                    rsl = ...;
+                if (number / 3 == 0 && number % 2 == 0) {
+                    rsl = "Исходное число не делится на 3, но является четным.";
                 } else {
-                    rsl = ...;
+                    rsl = "Исходное число не делится на 3 и не является четным.";
                 }
             }
             return rsl;
         }
-}
+
+        public static void main(String[] args) {
+            System.out.println(checkNumber(24));
+            System.out.println(checkNumber(9));
+            System.out.println(checkNumber(14));
+            System.out.println(checkNumber(25));
+        }
+    }
