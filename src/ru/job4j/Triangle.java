5139de61d45424d0a9868d48dfe0a3b05956acaf
AnreyUlitin <77831775+AnreyUlitin@users.noreply.github.com>
2021-02-16 09:18:51 +0300

    Update Triangle.java

--- src/ru/job4j/Triangle.java
+++ src/ru/job4j/Triangle.java
@@ -1,13 +1,9 @@
 package ru.job4j.Condition;
-
 public class Triangle {
 
     public static boolean exist(double ab, double ac, double bc) {
-        boolean result = ab + ac > bc;
-        boolean result1 = ac + bc > ab;
-        boolean result2 = ab + bc > ac;
 
-        return result && result2 && result2;
+        return ab + ac > bc && ac + bc > ab && ab + bc > ac;
     }
     public static void main(String[] args) {
         System.out.println(exist(2.0, 2.0, 4.5));
