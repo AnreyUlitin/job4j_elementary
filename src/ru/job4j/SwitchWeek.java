729936a01fe328cd3b90b514b6eff1d6ce42e5ed
AnreyUlitin <77831775+AnreyUlitin@users.noreply.github.com>
2021-02-16 22:47:25 +0300

    Add files via upload

---
+++ src/ru/job4j/SwitchWeek.java
@@ -0,0 +1,36 @@
+package ru.job4j.Condition;
+
+    public class SwitchWeek {
+        public static String nameOfDay(String day) {
+
+            switch (day) {
+                case int = 1 :
+                    nameOfDay = "Понедельник";
+                    break;
+                case int = 2:
+                    nameOfDay = "Вторник";
+                    break;
+                case int = 3:
+                    nameOfDay = "Третник";
+                    break;
+                case int = 4:
+                    nameOfDay = "Четверг";
+                    break;
+                case int = 5:
+                    nameOfDay = "Пятница";
+                    break;
+                case int = 6:
+                    nameOfDay = "Суббота";
+                    break;
+                case int = 7:
+                    nameOfDay = "Воскуресенье";
+                    break;
+                default:
+                    nameOfDay = "Восьмой день";
+                    break;
+            }
+            return new String("Ошибка");
+        }
+    }
+
+
