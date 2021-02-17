4f32d8edabae33617aa742ab5cd798045cdc46bf
AnreyUlitin <77831775+AnreyUlitin@users.noreply.github.com>
2021-02-17 13:16:41 +0300

    Update SwitchWeek.java

--- src/ru/job4j/SwitchWeek.java
+++ src/ru/job4j/SwitchWeek.java
@@ -1,36 +1,38 @@
 package ru.job4j.Condition;
 
     public class SwitchWeek {
-        public static String nameOfDay(String day) {
 
-            switch (day) {
-                case int = 1 :
-                    nameOfDay = "Понедельник";
-                    break;
-                case int = 2:
-                    nameOfDay = "Вторник";
-                    break;
-                case int = 3:
-                    nameOfDay = "Третник";
-                    break;
-                case int = 4:
-                    nameOfDay = "Четверг";
-                    break;
-                case int = 5:
-                    nameOfDay = "Пятница";
-                    break;
-                case int = 6:
-                    nameOfDay = "Суббота";
-                    break;
-                case int = 7:
-                    nameOfDay = "Воскуресенье";
-                    break;
-                default:
-                    nameOfDay = "Восьмой день";
-                    break;
-            }
-            return new String("Ошибка");
+    public static String nameOfDay(int day) {
+        String name;
+       
+        switch (day) {
+    case 1 :
+        name = "Понедельник";
+        break;
+    case 2:
+        name = "Вторник";
+        break;
+    case 3:
+        name = "Среда";
+        break;
+    case 4:
+        name = "Четверг";
+        break;
+    case 5:
+        name = "Пятница";
+        break;
+    case 6:
+        name = "Суббота";
+        break;
+    case 7:
+        name = "Воскуресенье";
+        break;
+    default:
+        name = "Ошибка";
+        break;
         }
+        return name;
     }
+}
 
 
