8b44d511e1b668974dbba27ab072e47fe5d86a2d
AnreyUlitin <77831775+AnreyUlitin@users.noreply.github.com>
2021-02-23 17:54:41 +0300

    Add files via upload

---
+++ src/ru/job4j/CheckPrimeNumber.java
@@ -0,0 +1,14 @@
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+
+        for (int index = 2; index <= index; index++) {
+            if ((index % 2) == 0) {
+                break;
+            }
+        }
+        boolean prime = true;
+        return prime;
+    }
+}
+
