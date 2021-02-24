19286f0eb1f955aa1950a47812f1fa645fd202e1
AnreyUlitin <77831775+AnreyUlitin@users.noreply.github.com>
2021-02-24 21:26:44 +0300

    Add files via upload

---
+++ src/ru/job4j/PrimeNumber.java
@@ -0,0 +1,18 @@
+package ru.job4j.ru.job4j.loop;
+
+public class PrimeNumber<CheckPrimeNumber> {
+
+    private static int CheckPrimeNumber;
+
+        (int finish)
+
+    public PrimeNumber() {
+        int count = 0;
+        for (int number = 1; count < finish; count++) {
+            if (PrimeNumber.CheckPrimeNumber.check(number)) {
+
+            }
+        }
+        return count;
+    }
+}
